8e9ddbbb12b316a70974d7d208a2462d1420cfb8
Ali Corbin <alicorbin91@yahoo.com>
2016-02-23 19:43:38 -0500

    Added w/v

--- Weight Volume Calcs.py
+++ Weight Volume Calcs.py
@@ -1,5 +1,5 @@
 Percent=float(input("What is the percent w/v?"))
-ML= int(input("What is the amount of solution?"))
+ML= int(input("What is the amount of solution in mL?"))
 Solution=(Percent*ML)
 Answer=(Solution/100)
 print (Answer)
